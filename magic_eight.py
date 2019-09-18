a57d812d226e6aa4f600a8a349721098b8977f1d
Rrrima <rimacyn@gmail.com>
2019-09-18 17:46:20 -0400

    add checking for question validation

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,10 @@
 def askq():
-	answer = input('What is your question?')
+	while(1):
+		question = input('What is your question?\n')
+		if question[-1] == '?':
+			break;
+		else:
+			print('Sorry, I can only answer questions.')
 	return answer
 
 askq()
